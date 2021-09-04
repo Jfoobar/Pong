4f52306e3f1ca10dc03c510205e5bf91bd531709
Jfoobar <jaydubwa@gmail.com>
2021-09-04 08:15:35 -0400

    ball bounces of brick, and removes brick

--- public/brick.mjs
+++ public/brick.mjs
@@ -1,20 +1,26 @@
+import {detectCollision} from "./collision.mjs"
 export default class Brick{
     constructor(game){
         this.image = document.getElementById("img_brick")
-        this.size = 45
+        this.width = 75
+        this.height = 45
         this.gameWidth = game.gameWidth
         this.gameHeight = game.gameHeight
 
         this.game = game 
 
-        this.position = {x:55, y:105}
+        this.position = {x:200, y:105}
     }
 
     draw(ctx){
-        ctx.drawImage(this.image, this.position.x, this.position.y,this.size,this.size)
+        ctx.drawImage(this.image, this.position.x, this.position.y,this.width,this.height)
     }
 
-    update(){
+    update(deltaTime){
+        if(detectCollision(this.game.ball, this)){
+            this.game.ball.speed.y = -this.game.ball.speed.y
+            this.width , this.height = 0
+        }
     }
 }
 
--- public/collision.mjs
+++ public/collision.mjs
@@ -1,13 +1,15 @@
 export const detectCollision = (ball,gameObject)=>{
+let ballBottom = ball.position.y + ball.size
+let ballTop = ball.position.y
+
 let obTop = gameObject.position.y
 let obBottom = gameObject.position.y + gameObject.height
 let obRside = gameObject.position.x + gameObject.width
 let obLside = gameObject.position.x
 
-if(ball.position.y + ball.size >= obTop
+if(ballBottom >= obTop
+   && ballTop  <= obBottom
    && ball.position.x >=obLside
-   && ball.position.y  <= obBottom
-   && ball.position.x >= obLside
    && ball.position.x + ball.size <= obRside
 ){
     return true
